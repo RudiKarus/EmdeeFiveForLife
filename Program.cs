bffea15a72a20e1a1e80600d8ebec7aceccf49e6
rudikarus <rudi@e-ld.de>
2024-01-25 20:29:49 +0000

    Added Cookie;

--- Program.cs
+++ Program.cs
@@ -3,44 +3,38 @@ using System.IO;
 using System.Text;
 using System.Net;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string address = "http://83.136.251.235:45095";
+        //SetProperties
+        WebClient webClient = new WebClient();
+        webClient.Headers.Add(HttpRequestHeader.Cookie, "PHPSESSID=vl38gua0b7idt8g2lgdt0oilp7");//from BurpSuite ;)
+        string address = "http://94.237.62.195:30254";
         string stringToReplace = "<h3 align='center'>";
         int lengthOfStringToEncode = 20;
-        //Register the CodePages encoding provider at application startup to enable using single and double byte encodings.
-        //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-        WebClient client = new WebClient();
-        string htmlContent = client.DownloadString(address);
+        //GetDataFromTarget
+        string htmlContent = webClient.DownloadString(address);
         string stringToEncode = GetStringToEncode(htmlContent, stringToReplace, lengthOfStringToEncode);
         string md5Hash = GetMd5Hash(stringToEncode);
 
-        #region POST_md5_hash_back_to_target
+        //PostMd5HashBackToTarget
         var data = new System.Collections.Specialized.NameValueCollection();
-        data["hash"] = md5Hash; //Send data back to the input field (name="hash") of the target
-        var response = client.UploadValues(address, "POST", data);
-        #endregion
+        data["hash"] = md5Hash;
+        var response = webClient.UploadValues(address, "POST", data);
 
-
-        #region LOG
+        //ConsoleLogging
         Console.WriteLine(htmlContent);
         Console.WriteLine("String to encode: " + stringToEncode);
-        Console.WriteLine($"MD5 hash: {md5Hash} \n");
+        Console.WriteLine($"Generated MD5 hash: {md5Hash} \n");
         File.WriteAllText(Environment.CurrentDirectory + "/md5hash.txt", md5Hash);
-        //Prompt the response in your local terminal:
         string responseString = System.Text.Encoding.ASCII.GetString(response);
         Console.WriteLine(responseString);
-        Console.WriteLine();
-        #endregion
     }
 
-
-    #region SERVICES
+    #region CrappyServices
     private static string GetStringToEncode(string htmlString, string patternToReplace, int lengthOfStringToEncode)
     {
         int startIndexOfPattern = htmlString.IndexOf(patternToReplace);
@@ -63,7 +57,6 @@ class Program
         byte[] inputBytes = Encoding.UTF8.GetBytes(stringToEncode);
         byte[] hashBytes = md5.ComputeHash(inputBytes);
         string md5Hash = Convert.ToHexString(hashBytes).ToLower();
-        // var md5Hash = Encoding.UTF8.GetString(hashBytes);
 
         return md5Hash;
     }
