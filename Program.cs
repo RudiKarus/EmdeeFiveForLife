c7115f6f62d68afcbee8734e48da642aa2b70eed
rudikarus <rudi@e-ld.de>
2024-01-25 11:42:34 +0000

    Refactoring MD5 hash;

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ class Program
         #region GET_target_html_and_replace_content
         //Get the html string from the target:
         WebClient client = new WebClient();
-        string address = "http://94.237.53.58:36883";
+        string address = "http://94.237.56.248:46467";
         string htmlContent = client.DownloadString(address);
         Console.WriteLine(htmlContent);
 
@@ -29,42 +29,30 @@ class Program
         Console.WriteLine("String to encode: " + stringToEncode);
         #endregion
 
+
+        #region GENERATE_MD5HASH
         //Register the CodePages encoding provider at application startup to enable using single and double byte encodings.
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
         //Generate the md5 hash:
         MD5 md5 = MD5.Create();
-
-        // byte[] inputBytes = Encoding.ASCII.GetBytes(stringToEncode);
-        // byte[] hashBytes = md5.ComputeHash(inputBytes);
-        // //var md5Hash = Convert.ToHexString(hashBytes).ToLower();
-        // var md5Hash = Encoding.ASCII.GetString(hashBytes);
-
-        // var byteArray = new byte[stringToEncode.Length];
-        // for (int i = 0; i < stringToEncode.Length; i++)
-        // {
-        //     byteArray[i] = (byte)stringToEncode[i];
-        // }
-        // byte[] hashBytes = md5.ComputeHash(byteArray);
-        // var asciiEncoding = Encoding.GetEncoding(437);
-        // string md5Hash = asciiEncoding.GetString(hashBytes);
-
-        //var asciiEncoding = Encoding.GetEncoding(437);
-        var asciiEncoding = Encoding.GetEncoding("iso-8859-1");
-        byte[] inputBytes = asciiEncoding.GetBytes(stringToEncode);
+        byte[] inputBytes = Encoding.UTF8.GetBytes(stringToEncode);
         byte[] hashBytes = md5.ComputeHash(inputBytes);
-        var md5Hash = asciiEncoding.GetString(hashBytes);
+        var md5Hash = Convert.ToHexString(hashBytes).ToLower();
+        // var md5Hash = Encoding.UTF8.GetString(hashBytes);
 
         //Generate a file and prompt the hash on your terminal:
         Console.WriteLine($"MD5 hash: {md5Hash} \n");
         File.WriteAllText(Environment.CurrentDirectory + "/md5hash.txt", md5Hash);
+        #endregion
+
 
         #region POST_md5_hash_back_to_target
         //Send data back to the input field (name="hash") of the target:
-        WebClient webClient = new WebClient();
+        //WebClient webClient = new WebClient();
         var data = new System.Collections.Specialized.NameValueCollection();
         data["hash"] = md5Hash;
-        var response = webClient.UploadValues(address, "POST", data);
+        var response = client.UploadValues(address, "POST", data);
 
         //Prompt the response in your local terminal:
         string responseString = System.Text.Encoding.ASCII.GetString(response);
---
+++ README.md
@@ -0,0 +1,13 @@
+#HacktTheBox Challenge: EmdeeFiveForLive
+https://app.hackthebox.com/challenges/emdee-five-for-life
+
+
+###HTTP Post
+https://www.delftstack.com/howto/csharp/csharp-post-request/
+
+###NugetPackages
+https://www.nuget.org/packages/System.Text.Encoding.CodePages/
+
+###Compare md5 hash using md5sum in bash (don't forget to cut the "newline" ending):
+$ echo <string> | tr -d '\n' > target.txt
+$ md5sum target.txt > md5hashFromTarget.txt
